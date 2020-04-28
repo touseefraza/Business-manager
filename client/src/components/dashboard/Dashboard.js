4237b36adbfe139b445151969319c6bbfb6bcf1e
touseefraza <touseefraza29@gmail.com>
2020-04-28 13:59:04 +0530

    initial commit

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import Background from '../../images/Dashboard_back.jpg'
+
+var sectionStyle = {
+  width: "100%",
+  height: "800px",
+  backgroundImage: "url(" + Background + ")"
+};
+class Dashboard extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+render() {
+    const { user } = this.props.auth;
+return (
+	<section style={ sectionStyle }>
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                 Your{" "}
+                <span style={{ fontFamily: "monospace" }}>Dashboard</span> is here
+              </p>
+            </h4>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+	  </section>
+    );
+  }
+}
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b class="red-text">Love</b> for your business, your <b>Business Manager</b> is here to gide you!{" "}
+             
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+             your business in your pocket. Get hold of your Business from anywhere and anytime
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large btn-flat waves-effect white black-text"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Landing;
